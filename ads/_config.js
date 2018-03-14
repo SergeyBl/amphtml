22da1ed8bf0d873b42cf9fadd383c496cce3de9c
unkvuzutop <sergey.blagun@imonomy.com>
2018-03-14 14:12:30 +0200

    imonomy fix tracking

--- ads/_config.js
+++ ads/_config.js
@@ -453,7 +453,6 @@ export const adConfig = {
     preconnect: 'https://spad.i-mobile.co.jp',
   },
   imonomy: {
-    prefetch: 'https://srv.imonomy.com/amp/amp.js',
     renderStartImplemented: true,
   },
   improvedigital: {},
